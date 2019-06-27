3cd6a91d53e0884e5449ceef48cc5681b6da31f5
Silvia Rosario Coro <mirian.mcp19@gmail.com>
2019-06-27 14:32:56 -0400

    ....

--- app/src/main/java/com/isoft/apptiendamovil/FormRegistro.java
+++ app/src/main/java/com/isoft/apptiendamovil/FormRegistro.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormRegistro extends AppCompatActivity {
+
+    private EditText nombres,apellidos,correo,fono;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,19 @@ public class FormRegistro extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        nombres = (EditText)findViewById(R.id.nombres);
+        apellidos = (EditText)findViewById(R.id.apellidos);
+        correo = (EditText)findViewById(R.id.correo);
+        fono = (EditText)findViewById(R.id.fono);
     }
 
+    public void btnSaveRegister(View view) {
+
+        GetServer ob = new GetServer();
+        //validación
+        String params="nombres="+nombres.getText()+"&apellidos="+apellidos.getText()+"&correo="+correo.getText()+"&fono="+fono.getText();
+        String cad=ob.stringQuery("/save_data?"+params,"POST");
+
+        Toast.makeText(getApplicationContext(),"Se registor su usuario",Toast.LENGTH_LONG).show();
+    }
 }
